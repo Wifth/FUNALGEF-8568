17577160f84d1603747ec923958ed1fae70d576a
Pierina01011ch <pierina21op15A@gmail.com>
2024-11-28 08:56:55 -0500

    primer commit

--- AlquilerCarros/CarroUtil.cs
+++ AlquilerCarros/CarroUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace AlquilerCarros
     {
         public static void CrearCarro()
         {
-
+            Pub
         }
         public static void ListarCarro()
         {
--- AlquilerCarros/ClienteUtil.cs
+++ AlquilerCarros/ClienteUtil.cs
@@ -8,17 +8,41 @@ namespace AlquilerCarros
 {
     public class ClienteUtil
     {
+        private static List<Cliente> Cliente = new List<Cliente>();
         public static void CrearCliente()
         {
-
+            Cliente nuevoCliente = new Cliente(DNI, nombre, edad);
+            clientes.Add(nuevoCliente);
+            Console.WriteLine($"Cliente{nombre} creado con éxito.");
         }
         public static void ListarCliente()
         {
-
+            if (clientes.Count == 0)
+            {
+                Console.WriteLine("No hay clientes en este sistema.");
+                return;
+            }
+            Console.WriteLine("Listado de clientes: ");
+            return;
+            foreach (var cliente in clientes)
+            {
+                Console.WriteLine($"DNI: {cliente.Dni}, Nombre: {cliente.Nombre}, PagoMaximo: {cliente.PagoMaximo}");
+            }
         }
         public static void EliminarCliente()
         {
+            bool clienteEliminado = false;
 
+            for (int i = 0; i < Cliente.Count; i++)
+            {
+                clientes.RemoveAt(i);
+                clienteEliminado = true;
+                Console.WriteLine($"Cliente con DNI {Dni} fue eliminado del sistema");
+                break;
+            }
+            if (int !clienteEliminado) {
+                Console.WriteLine($"No se encontró el cliente con el siguiente DNI {Dni}");
+            }
         }
     }
 }
